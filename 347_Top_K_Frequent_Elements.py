0348b21c903717bbfb2ff76a8f5209cbc5b7e4bf
Nav <arnavrgupta002@gmail.com>
2025-03-05 21:24:00 +0530

    Updated Top K solution

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -2,12 +2,35 @@
 Name: Top K Frequent Elements (#347)  
 URL: https://leetcode.com/problems/top-k-frequent-elements/  
 
-Time Complexity: O(N log N)  
+Time Complexity: 
+
+Sort: O(N * log N)  
+Heap: O(N * log K)
 Space Complexity: O(N)  
 """
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def heapSolution(self, nums, k):
+        freqMap = {}
+        minHeap = []
+
+        for num in nums:
+            if num not in freqMap:
+                freqMap[num] = 0
+
+            freqMap[num] += 1
+
+        # O(N * LogK)
+        for key in freqMap:
+            heappush(minHeap, (freqMap[key], key))
+
+            if len(minHeap) > k:
+                heappop(minHeap)
+
+        # O(K * LogK)
+        return [value for (frequency, value) in minHeap]
+
+    def sortSolution(self, nums, k):
         freq = {}
         freqArray = []
         res = []
@@ -27,3 +50,7 @@ class Solution:
             res.append(reverseSortedFreqArr[n][0])
 
         return res
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # return self.sortSolution(nums, k)
+        return self.heapSolution(nums, k)
