d83f87a8d7f4a77150808cb0760a0251dc5f2d82
Nav <arnavrgupta002@gmail.com>
2025-03-30 15:15:10 +0530

    DP

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -1,36 +1,55 @@
 """
 Name: Coin Change (#322)
 URL: https://leetcode.com/problems/coin-change/
-
-Time Complexity: O(M * N) [M: distinct states of remaining_amount]
-Space Complexity: O(N)
 """
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        cache = {}
+        cache = [float('inf')] * (amount + 1)
 
-        # ===
-        def min_number_of_coins(remaining_amount):
-            if remaining_amount in cache:
-                return cache[remaining_amount]
-
-            if remaining_amount < 0:
-                return float('inf')
-
+        """
+        Time Complexity: O(M * N) [M: distinct states of remaining_amount]
+        Space Complexity: O(M + M)
+        """
+        def memo_change(remaining_amount):
             if remaining_amount == 0:
                 return 0
+
+            if cache[remaining_amount] != float('inf'):
+                return cache[remaining_amount]
 
             min_coins = float('inf')
 
             for coin in coins:
-                num_coins = 1 + min_number_of_coins(remaining_amount - coin)
+                if coin > remaining_amount:
+                    continue
 
-                min_coins = min(num_coins, min_coins)
-
+                min_coins = min(min_coins, 1 + memo_change(remaining_amount - coin))
+            
             cache[remaining_amount] = min_coins
-            return min_coins
-        
-        # ===
-        num_changes = min_number_of_coins(amount)
-        return num_changes if num_changes != float('inf') else -1
+            return cache[remaining_amount]
+
+
+        """
+        Time Complexity: O(M * N) [M: distinct states of remaining_amount]
+        Space Complexity: O(M)
+        """
+        def tab_change():
+            cache[0] = 0
+
+            for amt in range(1, amount + 1):
+                for coin in coins:
+                    if coin > amt:
+                        continue
+
+                    cache[amt] = min(cache[amt], 1 + cache[amt - coin])
+
+            return cache[amount]
+
+        # Memoized
+        min_coins = memo_change(amount)
+        return min_coins if min_coins != float('inf') else -1
+
+        # Tabulated
+        min_coins = tab_change()
+        return min_coins if min_coins != float('inf') else -1
