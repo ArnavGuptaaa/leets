349bc23e16e59265d84f7c759f385471136ed99a
Nav <arnavrgupta002@gmail.com>
2025-04-01 23:46:16 +0530

    DP

--- 2140_Solving_Questions_With_Brainpower.py
+++ 2140_Solving_Questions_With_Brainpower.py
@@ -1,7 +1,27 @@
 """
 Name: Solving Questions With Brainpower (#2140)
-URL: <Add question link here>
+URL: https://leetcode.com/problems/solving-questions-with-brainpower/
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(N)
+Space Complexity: O(N + N)
 """
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        cache = [-1] * len(questions)
+
+        def dp(index):
+            if index >= len(questions):
+                return 0
+
+            if cache[index] != -1:
+                return cache[index]
+            
+            choose = questions[index][0] + dp(index + questions[index][1] + 1)
+            leave = dp(index + 1)
+
+            cache[index] = max(choose, leave)
+
+            return cache[index]
+
+        return dp(0)
